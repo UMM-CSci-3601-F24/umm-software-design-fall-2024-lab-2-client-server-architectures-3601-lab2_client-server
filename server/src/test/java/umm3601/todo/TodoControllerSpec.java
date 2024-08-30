74f5bbb643f702069f7f33f21e937a028c9dbba3
El <and10393@umn.edu>
2024-08-30 15:21:23 -0500

    Add Tests for todos
    Add applicable tests without red squiggles and some future tests that are commented out

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -26,11 +26,62 @@ import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
 import io.javalin.http.NotFoundResponse;
 import umm3601.Main;
+import umm3601.user.UserDatabase;
 
+@SuppressWarnings({"MagicNumber"})
 public class TodoControllerSpec {
   private TodoController todoController;
   private static TodoDatabase db;
+  @Mock
   private Context ctx;
+  @Captor
+  private ArgumentCaptor<Todo[]> todoArrayCaptor;
+
+
+  // @BeforeEach
+  // public void setUp() throws IOException {
+  //   MockitoAnnotations.openMocks(this);
+  //   db = new TodoDatabase(Main.TODO_DATA_FILE);
+  //   todoController = new TodoController(db);
+  // }
+
+
+  @Test
+  public void canBuildController() throws IOException{
+    TodoController controller = TodoController.buildTodoController(Main.TODO_DATA_FILE);
+    Javalin mockServer = Mockito.mock(Javalin.class);
+    controller.addRoutes(mockServer);
+    verify(mockServer, Mockito.atLeast(2)).get(any(), any());
+  }
+
+  // @Test
+  // public void buildControllerFailsWithIllegalDbFile(){
+  //   Assertions.assertThrows(IOException.class, () -> {
+  //     TodoController.buildTodoController("this is not a legal file name");
+  //   });
+  // }
+
+  @Test
+  public void canGetAllTodos() throws IOException {
+    todoController.getTodos(ctx);
+    verify(ctx).json(todoArrayCaptor.capture());
+    assertEquals(db.size(), todoArrayCaptor.getValue().length);
+  }
+
+  //this will have issues with the age
+  // @Test
+  // public void canGetTodosWithAge25() throws IOException {
+  //   Map<String, List<String>> queryParams = new HashMap<>();
+  //   queryParams.put("age",Arrays.asList(new String[] {"25"})); //age will cause issues
+  //   when(ctx.queryParamMap()).thenReturn(queryParams);
+  //   todoController.getUsers(ctx); //this will cause issues
+  //   verify(ctx).json(todoArrayCaptor.capture());
+  //   for(Todo todo : todoArrayCaptor.getValue()) {
+  //     assertEquals(25, todo.age); //will cause issues
+  //   }
+  //   assertEquals(2, todoArrayCaptor.getValue().length);
+  // }
+
 
   @Test
   public void canGetTodosByID() throws IOException {
